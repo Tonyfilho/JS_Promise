d20c0e1c66b6372f6002470308e8f8503b80f7f7
unknown <oficinadosdrones@gmail.com>
2022-01-19 17:25:08 +0000

    estudando FETCH

--- promise/src/app/app.module.ts
+++ promise/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PromiseConceitosBasicoComponent } from './promise_conceitos_basicos/promise_conceitos_basicos.component';
+import { PromiseCriadaPorInstanciaComponent } from './promise-criada-por-instancia/promise-criada-por-instancia.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PromiseConceitosBasicoComponent
+    PromiseConceitosBasicoComponent,
+    PromiseCriadaPorInstanciaComponent
   ],
   imports: [
     BrowserModule
---
+++ promise/src/app/promise-criada-por-instancia/promise-criada-por-instancia.component.ts
@@ -0,0 +1,76 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise-criada-por-instancia',
+  templateUrl: './promise-criada-por-instancia.component.html',
+  
+})
+export class PromiseCriadaPorInstanciaComponent implements OnInit {
+
+  constructor() {
+    /**
+     * Quando criamos uma PROMISE, temos criar uma FUNÇÂO e  passar os 2 parametros
+     * dentro da FUNÇÃO, que são eles (resolve, reject).
+     * A PROMISE recebe a FUNÇÃO e vai  gerencia ela, retornando
+     * SUCCESS or REJECT
+     * 
+     * Olhe o Ex: O RESOLVE É obrigatorio e o REJECT é opcional.
+     * 
+     * OBS: No JS quando não se colocar RETURN, nas Funções, seja PROMISE ou Não, 
+     * ele o JS RETORNA automaticamente UNDEFINED
+     * 
+     */
+     function promiseInstaciada(){
+       return new Promise((resolve, reject) => {
+         resolve('Ola Programador');
+         reject('Erro da conexão');
+
+       })
+     }
+     /**
+      * Testando a PROMISE
+      */
+      promiseInstaciada().then(data => console.log('PROMISSE ',data))
+
+      /**
+       * Ex de PROMISE que o Resolve vem depois que o SetimeOut for acionado depois 2 segundos
+       */
+
+      function promiseComSetimeOut(){
+        return new Promise((resolve, reject) => {
+          return setTimeout(() => {
+             resolve('Somente depois de 2S que o RESOLVE veio');
+           }, 2000);
+        return reject( console.log('Nosso error'));
+        })
+      }
+       /**
+      * Testando a PROMISE com SetimeOut()
+      */
+     promiseComSetimeOut().then(data => console.log(data));
+   }
+
+
+  ngOnInit(): void {
+    /**
+     * Neste Exemplo vamos usar uma PROMISE que vem de uma API
+     * o end da API é: https://viacep.com.br/ws/SEUCEP/json/
+     * ou seja vc manda seu CEP e recebe um JSON
+     * Varemos u REQUEST usando o Metodo FETCH,ele retorna uma promise
+     * OBS: o FETCH não pertence ao JS, mas sim há uma API do Browser
+     */
+  
+    function pegaCep(cep: any) {
+     return window.fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+    }
+    /**
+     * Chamando a função
+     */
+     pegaCep('01001000').then(data => { console.log(`1º DATA do FETCH devolve o Body que tb é JSON, por isto 
+      que temos que subscrever novamente em outra PROMISE data2 para recebermos o BODY`),
+    data.json().then(dataBody => console.log(dataBody))})
+  }
+
+}
