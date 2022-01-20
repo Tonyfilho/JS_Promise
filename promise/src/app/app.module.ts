5d853ffc6acab0da71b16b2f9e153b1bd0d7e147
unknown <oficinadosdrones@gmail.com>
2022-01-20 09:32:59 +0000

    Aula de Promise avançada

--- promise/src/app/app.module.ts
+++ promise/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PromiseConceitosBasicoComponent } from './promise_conceitos_basicos/promise_conceitos_basicos.component';
 import { PromiseCriadaPorInstanciaComponent } from './promise-criada-por-instancia/promise-criada-por-instancia.component';
+import { PromiseAvancadaEncadiamentoComponent } from './promise-avancada-encadiamento/promise-avancada-encadiamento.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PromiseConceitosBasicoComponent,
-    PromiseCriadaPorInstanciaComponent
+    PromiseCriadaPorInstanciaComponent,
+    PromiseAvancadaEncadiamentoComponent
   ],
   imports: [
     BrowserModule
---
+++ promise/src/app/promise-avancada-encadiamento/promise-avancada-encadiamento.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-promise-avancada-encadiamento',
+  templateUrl: './promise-avancada-encadiamento.component.html'  
+})
+export class PromiseAvancadaEncadiamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
